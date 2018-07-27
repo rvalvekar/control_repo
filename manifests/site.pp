b0f183a8af035e2e5a2f7580d214fc847ccf010b
rvalvekar <rvalvekar@gmail.com>
2018-07-27 05:47:52 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,12 @@
 node default {
-  file {'/root/README':
-    ensure => file,
-    content => 'this is a readme',
-    owner => 'root',
-  }
 }
 node 'master.puppet.vm' {
  include role::master_server
+ file {'/root/README':
+    ensure => file,
+    content => $fqdn,
+    owner => 'root',
+  }
 }
 node /^web/ {
  include role::app_server
